7cd7325faa522fd0ea3ad2b5d98c3cfdbfaf1245
Jia Hong Ye <hups900199@gmail.com>
2022-11-21 02:40:39 -0600

    Add description field in anime model.

--- db/migrate/20221116054627_create_animes.rb
+++ db/migrate/20221116054627_create_animes.rb
@@ -2,6 +2,7 @@ class CreateAnimes < ActiveRecord::Migration[7.0]
   def change
     create_table :animes do |t|
       t.string :name
+      t.string :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2022_11_16_060502) do
   create_table "animes", force: :cascade do |t|
     t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
