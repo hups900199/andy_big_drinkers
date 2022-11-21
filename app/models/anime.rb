7dcdd859f8aae85806b29fd061a87bd2af85e495
Jia Hong Ye <hups900199@gmail.com>
2022-11-21 02:41:51 -0600

    Increase name field length.

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -2,5 +2,5 @@ class Anime < ApplicationRecord
   has_many :images
 
   validates :name, presence: true, uniqueness: true
-  validates_length_of :name, minimum: 1, maximum: 70, allow_blank: false
+  validates_length_of :name, minimum: 1, maximum: 150, allow_blank: false
 end
