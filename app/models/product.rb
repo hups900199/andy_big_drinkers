88e2204a8d308ac78eeff5727d9c9ef4142b6e16
Jia Hong Ye <hups900199@gmail.com>
2022-11-21 23:10:15 -0600

    Fix image in models.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,6 @@ class Product < ApplicationRecord
   belongs_to :type
   belongs_to :image
 
-  has_one_attached :image
-
   validates :name, presence: true, uniqueness: true
   validates :stock, presence: true
   validates_length_of :name, minimum: 1, maximum: 100, allow_blank: false
--- app/models/type.rb
+++ app/models/type.rb
@@ -2,6 +2,8 @@ class Type < ApplicationRecord
   has_many :products
   has_many :images, through: :products
 
+  has_one_attached :image
+
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
   validates_length_of :name, minimum: 1, maximum: 70, allow_blank: false
