842a9297f567c19d4706fa6a84a8d5dd93b7d5f0
Jia Hong Ye <hups900199@gmail.com>
2022-11-21 22:55:42 -0600

    Add image in admin models.

--- app/admin/images.rb
+++ app/admin/images.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Image do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :price, :anime_id
+  permit_params :name, :price, :anime_id, :image
   #
   # or
   #
--- app/admin/types.rb
+++ app/admin/types.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Type do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :price
+  permit_params :name, :price, :image
   #
   # or
   #
