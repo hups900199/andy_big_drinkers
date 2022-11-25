134441f159642f9452717ca6d3af3620b7d73769
Jia Hong Ye <hups900199@gmail.com>
2022-11-24 21:03:15 -0600

    Add image in context model.

--- app/admin/contexts.rb
+++ app/admin/contexts.rb
@@ -6,6 +6,15 @@ ActiveAdmin.register Context do
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :title, :home, :about
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image, size:"200x200") : ""
+    end
+    f.actions
+  end
   #
   # or
   #
