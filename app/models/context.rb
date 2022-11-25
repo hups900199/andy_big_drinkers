a87683c24623f9d50030f19562db0f7bab1bed68
Jia Hong Ye <hups900199@gmail.com>
2022-11-24 20:58:45 -0600

    Add validation in context model.

--- app/models/context.rb
+++ app/models/context.rb
@@ -1,2 +1,7 @@
 class Context < ApplicationRecord
+  has_one_attached :image
+
+  validates :title, presence: true, uniqueness: true
+  validates :home, :about, presence: true
+  validates_length_of :title, minimum: 1, maximum: 70, allow_blank: false
 end
