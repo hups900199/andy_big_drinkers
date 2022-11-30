ce4863b6be230b11fca3050619c84f6d68d25446
Jia Hong Ye <hups900199@gmail.com>
2022-11-30 15:52:07 -0600

    Initialize session in application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_session
+  helper_method :cart
+
+  private
+
+  # Initialize the session for new users.
+  def initialize_session
+    # Empty array of product IDs
+    session[:shopping_cart] ||= []
+  end
+
+  def cart
+    # Pass an array of product ids and get back collection of products
+    Product.find(session[:shopping_cart])
+  end
 end
