09d87747270aeb9d251583df25fffaa555d89400
Jia Hong Ye <hups900199@gmail.com>
2022-12-01 22:24:38 -0600

    Generate order models and order item models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_27_103234) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_042232) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -90,6 +90,25 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_27_103234) do
     t.index ["anime_id"], name: "index_images_on_anime_id"
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.decimal "total"
+    t.decimal "unit_price"
+    t.integer "product_id", null: false
+    t.integer "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.decimal "subtotal"
+    t.decimal "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "stock"
@@ -125,6 +144,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_27_103234) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "images", "animes"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
   add_foreign_key "products", "images"
   add_foreign_key "products", "types"
 end
