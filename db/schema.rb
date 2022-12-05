ae5c50d159771eb45787630d51ac3e19d0ef0444
Jia Hong Ye <hups900199@gmail.com>
2022-12-05 17:51:48 -0600

    Generate migration to add province in user table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_03_193551) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_235032) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -146,7 +146,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_03_193551) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "province_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["province_id"], name: "index_users_on_province_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
@@ -157,4 +159,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_03_193551) do
   add_foreign_key "order_items", "products"
   add_foreign_key "products", "images"
   add_foreign_key "products", "types"
+  add_foreign_key "users", "provinces"
 end
