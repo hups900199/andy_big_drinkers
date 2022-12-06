cc240ada80a1224bf8c9fbcfcb0502599e94024c
Jia Hong Ye <hups900199@gmail.com>
2022-12-06 16:15:31 -0600

    Add admin view page.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,7 @@ class OrdersController < ApplicationController
   def index
     @orders = Order.where(user_id: current_user.id)
 
+    add_breadcrumb "All Orders", orders_admin_path if current_user.admin?
     add_breadcrumb "My Orders"
   end
 
@@ -12,7 +13,18 @@ class OrdersController < ApplicationController
 
     @line_items = Stripe::Checkout::Session.list_line_items(@order.payment)
 
+    add_breadcrumb "All Orders", orders_admin_path if current_user.admin?
     add_breadcrumb "My Orders", orders_index_path
     add_breadcrumb @order.id
+  end
+
+  def admin
+    @orders = if current_user.admin?
+                Order.where.not(status: [nil])
+              else
+                Order.where(user_id: current_user.id)
+              end
+
+    add_breadcrumb "All Orders"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
 
   get "orders/index"
   get "orders/show"
+  get "orders/admin"
 
   scope "/checkout" do
     post  "create",   to: "checkout#create",  as: "checkout_create"
